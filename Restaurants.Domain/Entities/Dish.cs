66a5c5edf51d6c35cced47994af6d1767658208f
Ravindu <ravindurasanjana1998@gmail.com>
2024-09-11 17:35:54 +0530

    dish update ef changes

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -5,6 +5,7 @@ public class Dish
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public decimal Price { get; set; }
+    public decimal KiloCalories { get; set; }
     public Guid RestaurantId { get; set; }
     public Restaurant Restaurant { get; set; } = null!;
 
---
+++ Restaurants.Infrastructure/Migrations/20240911120354_DishUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DishUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "KiloCalories",
+                table: "Dishes",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KiloCalories",
+                table: "Dishes");
+        }
+    }
+}
